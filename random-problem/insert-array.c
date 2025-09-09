53c73bb35b2cd06aa9578422f1f6beee8b8c0f62
MxAziz <hereabdulaziz@gmail.com>
2025-09-09 23:58:44 +0600

    Insert 5 elements into an array and print

---
+++ random-problem/insert-array.c
@@ -0,0 +1,15 @@
+// Program to insert elements into an array and display them
+
+#include <stdio.h>
+int main() {
+    int arr[5];
+    printf("Enter 5 elements: ");
+    for(int i=0; i<5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Array elements: ");
+    for(int i=0; i<5; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
