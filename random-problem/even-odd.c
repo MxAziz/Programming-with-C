5397de8a4e578aea3fab8df540a0286fbe2b2d9d
MxAziz <hereabdulaziz@gmail.com>
2025-09-09 23:48:55 +0600

    even or odd

---
+++ random-problem/even-odd.c
@@ -0,0 +1,14 @@
+
+// write a c program to print whether a given number is even or odd.?
+
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if(n % 2 == 0)
+        printf("Even\n");
+    else
+        printf("Odd\n");
+    return 0;
+}
