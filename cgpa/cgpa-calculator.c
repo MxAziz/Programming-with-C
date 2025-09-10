47e91c8af9cf4668c2ed36a9e5db644cbd9321e2
MxAziz <hereabdulaziz@gmail.com>
2025-09-10 22:52:56 +0600

    complete cgpa calculator

--- cgpa/cgpa-calculator.c
+++ cgpa/cgpa-calculator.c
@@ -7,7 +7,40 @@ int main() {
     printf("Enter number of subjects: ");
     scanf("%d", &n);
 
+    for (i = 1; i <= n; i++) {
+        printf("\nEnter marks of subject %d: ", i);
+        scanf("%d", &marks);
 
+
+        if (marks >= 80 && marks <= 100) {
+            printf("Grade: A+\n");
+            totalGP += 4.0;
+        }
+        else if (marks >= 70) {
+            printf("Grade: A\n");
+            totalGP += 3.5;
+        }
+        else if (marks >= 60) {
+            printf("Grade: A-\n");
+            totalGP += 3.0;
+        }
+        else if (marks >= 50) {
+            printf("Grade: B\n");
+            totalGP += 2.5;
+        }
+        else if (marks >= 40) {
+            printf("Grade: C\n");
+            totalGP += 2.0;
+        }
+        else if (marks >= 33) {
+            printf("Grade: D\n");
+            totalGP += 1.0;
+        }
+        else {
+            printf("Grade: F\n");
+            totalGP += 0.0;
+        }
+    }
 
     cgpa = totalGP / n;
 
